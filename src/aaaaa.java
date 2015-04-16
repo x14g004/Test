31e4240bb1fb6dcb6fa9a3843d120607ee2713aa
x14g004 <x14g004@PC505N004.chiba-fjb.ac.jp>
2015-04-16 15:02:21 +0900

    テスト

---
+++ src/aaaaa.java
@@ -0,0 +1,18 @@
+
+public class aaaaa {
+
+	public static void main(String[] args) {
+//		int i;
+//		//iを0に初期設定;i<3を満たしていたら処理を実行;処理終了後、iに1を足す
+//		for(i=0;i<3;i=i+1)  
+//		    System.out.println(i);  //iの値を出力し改行
+		
+		int[] a = {5,8,2,1,4};
+		int i;
+		for(i=0;i<a.length;i++)
+		{
+		    System.out.println(a[i]);
+		}
+	}
+
+}
